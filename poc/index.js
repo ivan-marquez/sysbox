de70b2471be607b3b08f33a1ae1fe339468ef1e9
Ivan Marquez <js.ivan.marquez@gmail.com>
2021-03-27 16:42:13 -0400

    add tmp PoC module
    Add a temporary module to test all desired features

---
+++ poc/index.js
@@ -0,0 +1,82 @@
+const si = require("systeminformation");
+const NetworkSpeed = require("network-speed");
+const perfmon = require("perfmon");
+
+// #region Clock
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  const strTime = hours + ":" + minutes + " " + ampm;
+
+  return strTime;
+}
+
+function formatTime(date) {
+  const hours = (d.getHours() < 10 ? "0" : "") + d.getHours();
+  const minutes = (d.getMinutes() < 10 ? "0" : "") + d.getMinutes();
+
+  return `${hours}:${minutes}`;
+}
+
+const time = si.time();
+const d = new Date(time.current);
+console.log(formatTime(d));
+console.log(formatAMPM(d));
+
+// #endregion
+
+// #region Docker info
+si.dockerAll((data) => console.log(data));
+// #endregion
+
+// #region Internet speed
+const testNetworkSpeed = new NetworkSpeed();
+
+async function getNetworkDownloadSpeed() {
+  const baseUrl = "https://eu.httpbin.org/stream-bytes/500000";
+  const fileSizeInBytes = 500000;
+  const speed = await testNetworkSpeed.checkDownloadSpeed(
+    baseUrl,
+    fileSizeInBytes
+  );
+  console.log(speed);
+}
+
+async function getNetworkUploadSpeed() {
+  const options = {
+    hostname: "www.google.com",
+    port: 80,
+    path: "/catchers/544b09b4599c1d0200000289",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const fileSizeInBytes = 2000000;
+  const speed = await testNetworkSpeed.checkUploadSpeed(
+    options,
+    fileSizeInBytes
+  );
+  console.log(speed);
+}
+
+getNetworkDownloadSpeed();
+getNetworkUploadSpeed();
+// #endregion
+
+// #region Performance counters
+const counters = [
+  "\\processor(_total)\\% processor time",
+  "\\memory\\Available MBytes",
+];
+
+perfmon(counters, function (_err, data) {
+  console.log(data);
+});
+// #endregion
